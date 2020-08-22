e22f7b7da628768cd1f527cdd6adaae9cfe387a1
towseefhossain <towseef.22aug@gmail.com>
2020-08-22 13:30:01 -0400

    Update git ignore and build script
    - Build Script to LF instead of CLRF
    - adding dist to gitignore

--- extension/script/build.sh
+++ extension/script/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 build() {
     echo "building react"
     rm -rf dist/*
@@ -8,7 +8,7 @@ build() {
     export GENERATE_SOURCEMAP=false
 
     react-scripts build
-    
+
     mkdir -p dist
     cp -r build/* dist
 
